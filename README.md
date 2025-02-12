7a8519dc6c37977aa089014701c65f3f7f8dac0f
jasonajordan <jasonajordan0@gmail.com>
2025-02-11 20:40:37 -0500

    added react router

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # foodieroute
 
 
-testing
+
+
+## How to launch the web app
+
+  cd webapp
+  npm install
+  npm run dev
+
+# You may need to run 
+npm install react-router-dom
+npm install firebase
+
+
--- webapp/src/main.jsx
+++ webapp/src/main.jsx
@@ -3,8 +3,34 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+// Pages
+// Basic pages
+import Bananas from './components/views/bananas.jsx';
+// Better pages (containers)
+
+// Router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />, // Homepage should be here
+    //ErrorPage: <ErrorPage />
+  },
+  {
+    path: "/banana",
+    element: <Bananas />,
+  }
+]);
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </StrictMode>,
 )
