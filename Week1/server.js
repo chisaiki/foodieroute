3cc1435c4c104e514787c46a7c5c20ef2326cd9d
tmensendiek61 <tomo.mensendiek61@myhunter.cuny.edu>
2025-02-13 12:50:25 -0500

    folders

--- Week1/server.js
+++ Week1/server.js
@@ -3,7 +3,7 @@ require('dotenv').config(); // Load .env file
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 8000; // You can change this to any port
+const port = 8000; 
 
 // Serve static files from the public folder
 app.use(express.static(path.join(__dirname, 'public')));
