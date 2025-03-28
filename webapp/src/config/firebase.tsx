5954999168963e67128888f5570efc5f6cac36f2
jasonajordan <jasonajordan0@gmail.com>
2025-03-28 14:01:13 -0400

    Regen the firebase key.

--- webapp/src/config/firebase.tsx
+++ webapp/src/config/firebase.tsx
@@ -8,17 +8,19 @@ import { getAuth, Auth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAxbI8q_Z_aljgR5hQgWyMdUvz32MHb5VY",
-  authDomain: "foodieroute.firebaseapp.com",
-  projectId: "foodieroute",
-  storageBucket: "foodieroute.firebasestorage.app",
-  messagingSenderId: "727835182381",
-  appId: "1:727835182381:web:ac8128fb9322921a56bfa4",
-  measurementId: "G-HRHTEPQQX8"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "foodieroute-46028.firebaseapp.com",
+  projectId: "foodieroute-46028",
+  storageBucket: "foodieroute-46028.firebasestorage.app",
+  messagingSenderId: "126786799246",
+  appId: "1:126786799246:web:2805c09e0d0a137e29abdf",
+  measurementId: "G-XVFTYG0FR4"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
