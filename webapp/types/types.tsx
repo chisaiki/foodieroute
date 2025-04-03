0d86a579c4c919709b853457abdd7f25659b6642
jasonajordan <jasonajordan0@gmail.com>
2025-04-03 19:55:19 -0400

    code clean up + overflow

--- webapp/types/types.tsx
+++ webapp/types/types.tsx
@@ -1,3 +1,5 @@
+
+// Places is not used anymore
 export type Places = {
   name: string;
   latitude: number;
@@ -10,3 +12,18 @@ export type Places = {
   summary?: string;
 };
 
+
+
+
+export type PriceLevel = "PRICE_LEVEL_FREE" | "PRICE_LEVEL_INEXPENSIVE" | "PRICE_LEVEL_MODERATE" | "PRICE_LEVEL_EXPENSIVE" | "PRICE_LEVEL_VERY_EXPENSIVE" | "PRICE_LEVEL_UNSPECIFIED";
+
+export type  Place = {
+  displayName?: { text: string };
+  location?: { latitude: number; longitude: number };
+  photos?: { name: string }[];
+  rating?: number;
+  priceLevel: PriceLevel;
+  formattedAddress?: string;
+  userRatingCount?: number;
+  editorialSummary?: { text: string };
+}
